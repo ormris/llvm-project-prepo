9350ffb4ee8f469a6cb7d590e91b23392bb453b7
Paul Bowen-Huggett <paulhuggett@mac.com>
2019-11-29 12:35:35 +0100

    Add repo-ticket-dump to the collection of tools being installed.

--- llvm/utils/repo/docker/build.sh
+++ llvm/utils/repo/docker/build.sh
@@ -5,6 +5,7 @@
     --install-target install-clang \
     --install-target install-pstore \
     --install-target install-repo2obj \
+    --install-target install-repo-ticket-dump \
     --install-target install-clang-headers \
     -- \
     -D CMAKE_BUILD_TYPE=Release
